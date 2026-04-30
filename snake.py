fff9d76bdb5c6a89738e4bd46e043e2331c07d93
MicahMurr <micahmurrayaustin@gmail.com>
2026-04-30 16:31:25 -0500

    done

--- snake.py
+++ snake.py
@@ -1,6 +1,8 @@
 from turtle import *
 import random
 
+body = []
+
 def playing_area():
     pen = Turtle()
     pen.ht()
@@ -19,8 +21,9 @@ def playing_area():
 class Head(Turtle):
     def __init__(self, screen):
         super().__init__()
-        self.shape("square")
+        self.shape("arrow")
         self.color("green")
+        self.speed(0)
         self.penup()
         self.goto(0, 0)
         self.direction = "stop"
@@ -58,14 +61,50 @@ class Head(Turtle):
         if self.xcor() > 240 or self.xcor() < -240 or self.ycor() > 240 or self.ycor() < -240:
             self.hideturtle()
             self.alive = False
-
+    def collision(self):
+        for i in body:
+            if i.distance(head)<20:
+                self.alive=False
 class Apple(Turtle):
     def __init__(self):
         super().__init__()
+        self.ht()
+        self.speed(0)
         self.shape("circle")
         self.color("red")
         self.penup()
         self.goto(random.randint(-230, 230), random.randint(-230, 230))
+        self.st()
+
+class Segment(Turtle):
+    def __init__(self, other):
+        super().__init__()
+        self.speed(0)
+        self.shape("square")
+        self.color("green")
+        self.penup()
+        self.goto(other.pos())
+        body.append(self)
+
+    def move(self, other):
+        for i in range(len(body) - 1, 0, -1):
+            x = body[i - 1].xcor()
+            y = body[i - 1].ycor()
+            body[i].goto(x, y)
+            
+        x = other.xcor()
+        y = other.ycor()
+        body[0].goto(x, y)
+
+def update():
+    if head.alive:
+        if head.distance(apple) < 20:
+            apple.goto(random.randint(-230, 230), random.randint(-230, 230))
+            Segment(head)
+        if len(body)>0:
+            body[0].move(head)
+        head.move()
+    screen.ontimer(update, 30)
 
 screen = Screen()
 screen.bgcolor("black")
@@ -73,13 +112,14 @@ screen.setup(520, 520)
 screen.listen()
 
 playing_area()
-
 head = Head(screen)
 apple = Apple()
 
-while head.alive:
-    head.move()
-    if head.distance(apple) < 20:
-        apple.goto(random.randint(-230, 230), random.randint(-230, 230))
-
+update()
+collision()
 screen.exitonclick()
+
+
+
+
+
